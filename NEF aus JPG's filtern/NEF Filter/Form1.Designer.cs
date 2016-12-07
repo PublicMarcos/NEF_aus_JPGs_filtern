12d851f100696a66987a2c430c96ed223baf9e19
Marcos <PublicMarcos@web.de>
2016-12-07 17:09:24 +0100

    #1

---
+++ NEF aus JPG's filtern/NEF Filter/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace NEF_Filter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PathLabel = new System.Windows.Forms.Label();
+            this.PathTextBox = new System.Windows.Forms.TextBox();
+            this.SearchPathButton = new System.Windows.Forms.Button();
+            this.FilterButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // PathLabel
+            // 
+            this.PathLabel.AutoSize = true;
+            this.PathLabel.Location = new System.Drawing.Point(12, 25);
+            this.PathLabel.Name = "PathLabel";
+            this.PathLabel.Size = new System.Drawing.Size(29, 13);
+            this.PathLabel.TabIndex = 0;
+            this.PathLabel.Text = "Pfad";
+            // 
+            // PathTextBox
+            // 
+            this.PathTextBox.BackColor = System.Drawing.SystemColors.Window;
+            this.PathTextBox.Location = new System.Drawing.Point(47, 22);
+            this.PathTextBox.Name = "PathTextBox";
+            this.PathTextBox.ReadOnly = true;
+            this.PathTextBox.Size = new System.Drawing.Size(301, 20);
+            this.PathTextBox.TabIndex = 1;
+            // 
+            // SearchPathButton
+            // 
+            this.SearchPathButton.Location = new System.Drawing.Point(354, 20);
+            this.SearchPathButton.Name = "SearchPathButton";
+            this.SearchPathButton.Size = new System.Drawing.Size(79, 23);
+            this.SearchPathButton.TabIndex = 2;
+            this.SearchPathButton.Text = "Durchsuchen";
+            this.SearchPathButton.UseVisualStyleBackColor = true;
+            this.SearchPathButton.Click += new System.EventHandler(this.SearchPathButton_Click);
+            // 
+            // FilterButton
+            // 
+            this.FilterButton.Enabled = false;
+            this.FilterButton.Location = new System.Drawing.Point(439, 20);
+            this.FilterButton.Name = "FilterButton";
+            this.FilterButton.Size = new System.Drawing.Size(79, 23);
+            this.FilterButton.TabIndex = 3;
+            this.FilterButton.Text = "Filtern";
+            this.FilterButton.UseVisualStyleBackColor = true;
+            this.FilterButton.Click += new System.EventHandler(this.FilterButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(534, 61);
+            this.Controls.Add(this.FilterButton);
+            this.Controls.Add(this.SearchPathButton);
+            this.Controls.Add(this.PathTextBox);
+            this.Controls.Add(this.PathLabel);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "NEF aus JPG filtern";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label PathLabel;
+        private System.Windows.Forms.TextBox PathTextBox;
+        private System.Windows.Forms.Button SearchPathButton;
+        private System.Windows.Forms.Button FilterButton;
+    }
+}
+
---
+++ NEF aus JPG's filtern/NEF Filter/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace NEF_Filter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private List<string> AllJPGs = new List<string>();
+        private List<string> AllNEFs = new List<string>();
+
+        private void SearchPathButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog objDialog = new FolderBrowserDialog();
+            objDialog.Description = "Ordner mit JPG's und NEF's auswählen";
+            objDialog.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            DialogResult objResult = objDialog.ShowDialog(this);
+            if (objResult == DialogResult.OK)
+            {
+                AllJPGs = Directory.GetFiles(objDialog.SelectedPath, "*.JPG", SearchOption.TopDirectoryOnly).ToList();
+                AllNEFs = Directory.GetFiles(objDialog.SelectedPath, "*.NEF", SearchOption.TopDirectoryOnly).ToList();
+                if (AllJPGs.Count > 1 && AllNEFs.Count > 1)
+                {
+                    for (int i = 0; i < AllJPGs.Count; i++)
+                    {
+                        AllJPGs[i] = Path.GetFileNameWithoutExtension(AllJPGs[i]);
+                    }
+                    FilterButton.Enabled = true;
+                    PathTextBox.Text = objDialog.SelectedPath;
+                }
+                else
+                {
+                    FilterButton.Enabled = false;
+                    PathTextBox.Text = string.Empty;
+                }
+            }
+        }
+
+        private void FilterButton_Click(object sender, EventArgs e)
+        {
+            FilterButton.Enabled = false;
+            for (int i = 0; i < AllNEFs.Count; i++)
+            {
+                if (!AllJPGs.Contains(Path.GetFileNameWithoutExtension(AllNEFs[i])))
+                {
+                    try
+                    {
+                        File.Delete(AllNEFs[i]);
+                    } catch { }
+                }
+            }
+            FilterButton.Enabled = true;
+            MessageBox.Show("Die Bilder wurden erfolgreich gefiltert.");
+        }
+
+    }
+}
